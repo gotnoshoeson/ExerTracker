da57606f91b55a3a70eae7ca3badd71863da45c6
gotnoshoeson <miles_patterson@icloud.com>
2022-06-21 16:57:24 -0700

    Almost done. Need to fix the edit Action in the Logged Exercises route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
+import EditExercises from "./components/edit-exercise.component";
 import CreateExercises from "./components/create-exercise.component";
 import CreateUsers from "./components/create-user.component";
 
@@ -16,7 +16,7 @@ function App() {
       <br/>
       <Routes>
         <Route path="/" exact element={<ExercisesList />} />
-        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/edit/:id" element={<EditExercises />} />
         <Route path="/create" element={<CreateExercises />} />
         <Route path="/user" element={<CreateUsers />} />
       </Routes>
